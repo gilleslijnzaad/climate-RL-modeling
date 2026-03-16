ba8c0b2b6a08561820295225ec2e3be6298640b9
Gilles Lijnzaad <gilleslijnzaad@gmail.com>
2026-03-16 17:35:11 +0100

    added all conf_bias of LRN type to shiny app

--- R_simulation/shiny_app/app.R
+++ R_simulation/shiny_app/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(bslib)
 
 options(shiny.autoreload.legacy_warning = FALSE)
 options(shiny.fullstacktrace = FALSE)
@@ -6,23 +7,64 @@ options(shiny.fullstacktrace = FALSE)
 ui <- fluidPage(
   titlePanel("Climate-RL simulation"),
   sidebarLayout(
+
     # INPUTS
     sidebarPanel(
-      numericInput(inputId = "n_part",
-                   label = "Number of participants:",
-                   min = 1,
-                   value = 50),
-      numericInput(inputId = "n_trials",
-                   label = "Number of trials:",
-                   min = 1,
-                   value = 30),
-      sliderInput(inputId = "LR",
-                  label = "Learning rate:",
-                  min = 0,
-                  max = 1,
-                  value = 0.4,
-                  step = 0.1,
-                  ticks = FALSE),
+      width = 3, # out of a total 12 units
+      style = "height: 89vh; overflow-y: auto;", # sidebar scroll
+
+      # EXPERIMENT SETTINGS
+      tags$h3("Experiment settings"),
+      numericInput("n_part", "Number of participants:", min = 1, value = 50),
+      numericInput("n_trials", "Number of trials:", min = 1, value = 30),
+      hr(),
+
+      # SIMULATION TYPE
+      tags$h3("Simulation type"),
+      radioButtons("conf_bias", "Confirmation bias",
+                   c("None (standard model)" = "none",
+                    "Learning level (LRN)" = "LRN")
+      ),
+      conditionalPanel(condition = "input.conf_bias == 'LRN'",
+        radioButtons("d_or_c", "Bias is:",
+                     c("Discrete" = "discr", "Continuous" = "cont")),
+        conditionalPanel(condition = "input.d_or_c == 'discr'",
+          radioButtons("confirmatory", "An outcome is confirmatory when:",
+                       c("R ≈ belief" = "approx", "R ≥ belief" = "geq"))
+        ),
+        radioButtons("beliefs", "Beliefs are:",
+                     c("Static" = "stat", "Dynamic" = "dyn"))
+      ),
+      hr(),
+      
+      # PARAMETER SETTINGS
+      tags$h3("Parameter settings"),
+      # learning rate
+      conditionalPanel(condition = "input.conf_bias == 'none'",
+        sliderInput("LR", "Learning rate:",
+                    min = 0, max = 1,
+                    value = 0.4, step = 0.1,
+                    ticks = FALSE)
+      ),
+      conditionalPanel(condition = "input.conf_bias == 'LRN' &&
+                                    input.d_or_c == 'discr'",
+        sliderInput("LR_conf", "Confirmatory learning rate:",
+                    min = 0, max = 1,
+                    value = 0.8, step = 0.1,
+                    ticks = FALSE),
+        sliderInput("LR_disconf", "Disconfirmatory learning rate:",
+                    min = 0, max = 1,
+                    value = 0.2, step = 0.1,
+                    ticks = FALSE)
+      ),
+      conditionalPanel(condition = "input.conf_bias == 'LRN' &&
+                                    input.d_or_c == 'cont'",
+        sliderInput("w_LR", "Weight for learning rate",
+                    min = 0, max = 1,
+                    value = 0.4, step = 0.1,
+                    ticks = FALSE)
+      ),
+      
       sliderInput(inputId = "inv_temp",
                   label = "Inverse temperature:",
                   min = 0,
@@ -60,7 +102,12 @@ ui <- fluidPage(
                   max = 5,
                   value = 2,
                   ticks = FALSE),
-
+      conditionalPanel(condition = "input.conf_bias == 'LRN' &&
+                                    input.d_or_c == 'discr'",
+        sliderInput("margin", "Margin:",
+                    min = 0, max = 5,
+                    value = 2, ticks = FALSE)
+      ),
     ),
     # OUTPUT
     mainPanel(
@@ -70,33 +117,80 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  sim <- new.env()
-  source("../sim.R", local = sim)
-
   params <- reactive({
-    list(
+    p <- list(
       n_part = input$n_part,
       n_trials = input$n_trials,
-      LR = input$LR,
       inv_temp = input$inv_temp,
       initQF = input$initQF,
       initQU = input$initQU,
-      mu_R = c(input$mu_R_F, input$mu_R_U), # F and U
+      mu_R = c(input$mu_R_F, input$mu_R_U),
       sigma_R = input$sigma_R
     )
+
+    if (input$conf_bias == 'none') {
+      p$LR <- input$LR
+    }
+    if (input$conf_bias == 'LRN') {
+      if (input$d_or_c == 'discr') {
+        p$LRs = list(conf = input$LR_conf, disconf = input$LR_disconf)
+        p$margin = input$margin
+      }
+      if (input$d_or_c == 'cont') {
+        p$w_LR = input$w_LR
+      }
+    }
+    p
   })
 
-  sim_dat <- reactive({
-    sim$run_std(params())
+  sim <- new.env()
+  source("../sim.R", local = sim)
+
+  dat <- reactive({
+    if (input$conf_bias == 'none') {
+      d <- sim$run_std(params())
+    }
+    if (input$conf_bias == 'LRN') {
+      if (input$d_or_c == 'discr') {
+        if (input$confirmatory == 'approx') {
+          LR_fun <- sim$LR_approx
+        }
+        if (input$confirmatory == 'geq') {
+          LR_fun <- sim$LR_geq
+        }
+        d <- sim$run_LRN_discr(params(), LR_fun, input$beliefs)
+      }
+      if (input$d_or_c == 'cont') {
+        d <- sim$run_LRN_cont(params(), input$beliefs)
+      }
+    }
+    d
   })
 
   plot <- new.env()
   source("../../plot_utils.R", local = plot)
+
+  plot_title <- reactive({
+    t <- "Simulation type: "
+    if (input$conf_bias == 'none') {
+      t <- paste0(t, "std")
+    } 
+    if (input$conf_bias == 'LRN') {
+      t <- paste0(t, "LRN")
+      if (input$d_or_c == "discr") {
+        t <- paste(t, input$d_or_c, input$confirmatory, input$beliefs, sep = "_")
+
+      }
+      if (input$d_or_c == "cont") {
+        t <- paste(t, input$d_or_c, input$beliefs, sep = "_")
+      }
+    }
+    t
+  })
   
   output$plot <- renderPlot({
-    plot$sim_plots(sim_dat(), params())
+    plot$sim_plots(dat(), params(), plot_title())
  })
 }
 
-# Create Shiny app ----
 shinyApp(ui = ui, server = server)
