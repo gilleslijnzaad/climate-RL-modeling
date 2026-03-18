54f3fb1e6243883b3c0e11b47610283bc693faa1
Gilles Lijnzaad <gilleslijnzaad@gmail.com>
2026-03-18 15:37:33 +0100

    moved cred_int() and print_posterior_table() to their own file

---
+++ utils.R
@@ -0,0 +1,48 @@
+# === cred_int() ========================
+# arguments: 
+# - posterior_dist: array of posterior distribution
+# 
+# returns: 
+# - bounds of 95% credibility interval in array
+cred_int <- function(posterior_dist) {
+  return(as.numeric(quantile(posterior_dist, c(0.025, 0.975))))
+}
+
+# === print_posterior_table() ========================
+# arguments: 
+# - draws: data frame of posterior draws from model
+# - to_show: string array of parameters to show in table
+# - param_settings: named list of parameter settings
+# 
+# returns: 
+# - nothing
+print_posterior_table <- function(draws, to_show, param_settings) {
+  table_data <- data.frame()
+
+  for (p in to_show) {
+    if (grepl("\\$", p)) { # parameter is part of a list
+      split_name <- strsplit(p, "\\$")[[1]]
+      sim_value <- purrr::pluck(param_settings, split_name[1], split_name[2])
+      param_name <- paste0(split_name[1], "\\$", split_name[2]) # escape dollar sign for kable
+    } else {
+      sim_value <- param_settings[[p]]
+      param_name <- p
+    }
+    dat <- data.frame(
+      parameter = param_name,
+      sim_value = sim_value,
+      median_CI = sprintf("%.2f [%.2f, %.2f]",
+                          median(draws[[p]]),
+                          cred_int(draws[[p]])[1],
+                          cred_int(draws[[p]])[2])
+    )
+    table_data <- rbind(table_data, dat)
+  }
+  colnames <- c("Parameter", "Simulated value",
+                "Median [95% credibility interval]")
+  knitr::kable(table_data,
+               col.names = colnames,
+               align = "lll",
+               caption = "Posteriors for all free parameters") %>%
+  kableExtra::kable_styling(full_width = FALSE, position = "left")
+}
