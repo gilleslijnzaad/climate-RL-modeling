bb5341c3d95f3af95529de99140ceabbd70f4f40
Gilles Lijnzaad <gilleslijnzaad@gmail.com>
2026-04-29 17:28:10 +0200

    did single run for 2_LRN_regular

--- 0_models/2_LRN_discr_approx_stat.stan
+++ 0_models/2_LRN_discr_approx_stat.stan
@@ -14,8 +14,8 @@ parameters {
   vector<lower=0>[5] sigmas;
 
   // participant-level parameters
-  vector[n_part] LR_conf_probit;
   vector[n_part] LR_disconf_probit;
+  vector[n_part] LR_diff_probit;
   vector[n_part] inv_temp_probit;
   vector[n_part] initQF_probit;
   vector[n_part] initQU_probit;
@@ -23,15 +23,15 @@ parameters {
 
 transformed parameters {
   // participant-level parameters
-  vector<lower=0, upper=1>[n_part] LR_conf; 
   vector<lower=0, upper=1>[n_part] LR_disconf; 
+  vector<lower=0, upper=1>[n_part] LR_diff; 
   vector<lower=0, upper=5>[n_part] inv_temp;
   vector<lower=1, upper=10>[n_part] initQF;
   vector<lower=1, upper=10>[n_part] initQU;
 
   for (j in 1:n_part) {
-    LR_conf[j] = Phi_approx(means_probit[1] + sigmas[1] * LR_conf_probit[j]);
-    LR_disconf[j] = Phi_approx(means_probit[2] + sigmas[2] * LR_disconf_probit[j]);
+    LR_disconf[j] = Phi_approx(means_probit[1] + sigmas[1] * LR_disconf_probit[j]);
+    LR_diff[j] = Phi_approx(means_probit[2] + sigmas[2] * LR_diff_probit[j]);
     inv_temp[j] = Phi_approx(means_probit[3] + sigmas[3] * inv_temp_probit[j]) * 5;
     initQF[j] = Phi_approx(means_probit[4] + sigmas[4] * initQF_probit[j]) * 9 + 1;
     initQU[j] = Phi_approx(means_probit[5] + sigmas[5] * initQU_probit[j]) * 9 + 1;
@@ -45,8 +45,8 @@ model {
   means_probit ~ normal(0, 1);
   sigmas ~ normal(0, 0.2);
   
-  LR_conf_probit ~ normal(0, 1);
   LR_disconf_probit ~ normal(0, 1);
+  LR_diff_probit ~ normal(0, 1);
   inv_temp_probit ~ normal(0, 1);
   initQF_probit ~ normal(0, 1); 
   initQU_probit ~ normal(0, 1); 
@@ -78,7 +78,7 @@ model {
         // choice is F
         if (choice[j, t] == 1) {
           if (abs(R[j, t] - initQF[j]) <= margin) {
-            LR = LR_conf[j];
+            LR = LR_disconf[j] + LR_diff[j]; // confirmatory
           } else {
             LR = LR_disconf[j];
           }
@@ -89,7 +89,7 @@ model {
         // choice is U
         else {
           if (abs(R[j, t] - initQU[j]) <= margin) {
-            LR = LR_conf[j];
+            LR = LR_disconf[j] + LR_diff[j]; // confirmatory
           } else {
             LR = LR_disconf[j];
           }
@@ -103,9 +103,9 @@ model {
 
 generated quantities {
   vector[5] means;
-  means[1] = Phi_approx(means_probit[1]);
-  means[2] = Phi_approx(means_probit[2]);
-  means[3] = Phi_approx(means_probit[3]) * 5;
-  means[4] = Phi_approx(means_probit[4]) * 9 + 1;
-  means[5] = Phi_approx(means_probit[5]) * 9 + 1;
+  means[1] = Phi_approx(means_probit[1]); // LR_disconf_group
+  means[2] = Phi_approx(means_probit[2]); // LR_diff_group
+  means[3] = Phi_approx(means_probit[3]) * 5; // inv_temp_group
+  means[4] = Phi_approx(means_probit[4]) * 9 + 1; // initQF_group
+  means[5] = Phi_approx(means_probit[5]) * 9 + 1; // initQU_group
 }
--- 3_param_recovery/run_param_recovery.R
+++ 3_param_recovery/run_param_recovery.R
@@ -19,83 +19,77 @@ source(paste0(util_dir, "utils.R"), local = util)
 # ------------------------------------------------------------
 # SIM + FIT SINGLE -------------------------------------------
 # ------------------------------------------------------------
-params <- list(
-  n_part = 50,
-  n_trials = 30,
-  LRs_group = list(disconf = 0.2, diff = 0.6),
-  inv_temp_group = 0.5,
-  initQF_group = 8,
-  initQU_group = 2,
-  mu_R_group = list(F = 5, U = 5),
-  sigma_R_group = 2,
-  margin_group = 2
-)
+if (TRUE) {
+  params <- list(
+    n_part = 50,
+    n_trials = 30,
+    LRs_group = list(disconf = 0.2, diff = 0.6),
+    inv_temp_group = 0.5,
+    initQF_group = 8,
+    initQU_group = 2,
+    mu_R_group = list(F = 5, U = 5),
+    sigma_R_group = 2,
+    margin_group = 2
+  )
 
-# SIM
-sim_dat <- sim$run(params)
+  # SIM
+  sim_dat <- sim$run(params)
 
-current_dir <- paste0(recov_dir, mod_name, "/simple/")
-dat_dir <- paste0(current_dir, "1_run/")
-dat_path <- paste0(dat_dir, "sim_dat_1.json")
-free_params_pp <- c("LR_disconf", "LR_diff", "inv_temp", "initQF", "initQU")
-sim_utils$save_sim_dat(params, sim_dat, dat_path, free_params_pp)
+  current_dir <- paste0(recov_dir, mod_name, "/regular/")
+  dat_dir <- paste0(current_dir, "1_run/")
+  dat_path <- paste0(dat_dir, "sim_dat_1.json")
+  free_params_pp <- c("LR_disconf", "LR_diff", "inv_temp", "initQF", "initQU")
+  sim_utils$save_sim_dat(params, sim_dat, dat_path, free_params_pp)
 
-# OPTIONAL: ADD PARAMS TO DATA
-# add initQF[] and initQU[] to json data
-initQFs <- sim_dat$Q_F[which(sim_dat$trial == 1)]
-initQUs <- sim_dat$Q_U[which(sim_dat$trial == 1)]
-json_dat <- rjson::fromJSON(file = dat_path)
-json_dat[["initQF"]] <- initQFs
-json_dat[["initQU"]] <- initQUs
-cmdstanr::write_stan_json(json_dat, file = dat_path)
+  plot$sim_plots(sim_dat, params)
 
-plot$sim_plots(sim_dat, params)
-
-# FIT
-model_path <- paste0(mod_dir, mod_name, "_simple.stan")
-model <- cmdstan_model(model_path)
-# draws <- fitting$fit(model, dat_path, return = "draws", show_iteration_progress = TRUE)
-# saveRDS(draws, paste0(dat_dir, "draws.rds"))
-draws <- readRDS(paste0(dat_dir, "draws.rds"))
-free_params <- c("LR_disconf_group", "LR_diff_group", "inv_temp_group")
-draws <- draws %>% 
-  rename(setNames(paste0("means[", seq_along(free_params), "]"),
-                  free_params))
-
+  # FIT
+  model_path <- paste0(mod_dir, mod_name, ".stan")
+  model <- cmdstan_model(model_path)
+  draws <- fitting$fit(model, dat_path, return = "draws", show_iteration_progress = TRUE)
+  saveRDS(draws, paste0(dat_dir, "draws.rds"))
+  draws <- readRDS(paste0(dat_dir, "draws.rds"))
+  free_params <- c("LR_disconf_group", "LR_diff_group", "inv_temp_group", "initQF_group", "initQU_group")
+  draws <- draws %>% 
+    rename(setNames(paste0("means[", seq_along(free_params), "]"),
+                    free_params))
+}
 # INSPECT
-to_plot <- list(c("LR_disconf_group", "LR_diff_group"), "inv_temp_group")
+to_plot <- list(c("LR_disconf_group", "LR_diff_group"), "inv_temp_group", c("initQF_group", "initQU_group"))
 plot$posterior_densities(draws, to_plot, params)
 
 # ------------------------------------------------------------
 # SIM + FIT MANY ---------------------------------------------
 # ------------------------------------------------------------
-n_runs <- 100
-dat_dir <- paste0(current_dir, "100_runs/")
-sim$run_many(params, dat_dir, n_runs)
-# running this bit below takes at least an hour
-# fitting$fit_many(dat_dir, model_path, fit_data_type = "draws", dat_dir, n_runs)
+if (FALSE) {
+  n_runs <- 100
+  dat_dir <- paste0(current_dir, "100_runs/")
+  sim$run_many(params, dat_dir, n_runs)
+  # running this bit below takes at least an hour
+  # fitting$fit_many(dat_dir, model_path, fit_data_type = "draws", dat_dir, n_runs)
 
-# INSPECT
-sim_params <- data.frame(k = 1:n_runs)
-fit_params <- data.frame(k = 1:n_runs)
+  # INSPECT
+  sim_params <- data.frame(k = 1:n_runs)
+  fit_params <- data.frame(k = 1:n_runs)
 
-for (k in 1:n_runs) {
-  sim_file <- paste0(dat_dir, "param_settings_", sprintf("%03d", k), ".json")
-  sim_dat <- rjson::fromJSON(file = sim_file)
-  if ("LRs_group" %in% names(sim_dat)) {
-    sim_dat[["LR_disconf_group"]] <- sim_dat[["LRs_group"]][1]
-    sim_dat[["LR_diff_group"]] <- sim_dat[["LRs_group"]][2]
+  for (k in 1:n_runs) {
+    sim_file <- paste0(dat_dir, "param_settings_", sprintf("%03d", k), ".json")
+    sim_dat <- rjson::fromJSON(file = sim_file)
+    if ("LRs_group" %in% names(sim_dat)) {
+      sim_dat[["LR_disconf_group"]] <- sim_dat[["LRs_group"]][1]
+      sim_dat[["LR_diff_group"]] <- sim_dat[["LRs_group"]][2]
+    }
+
+    fit_file <- paste0(dat_dir, "draws_", sprintf("%03d", k), ".rds")
+    fit_dat <- readRDS(fit_file) %>%
+      rename(setNames(paste0("means[", seq_along(free_params), "]"),
+                      free_params))
+
+    for (p in free_params) {
+      sim_params[[p]][k] <- sim_dat[[p]]
+      fit_params[[p]][k] <- median(fit_dat[[p]])
+    }
   }
 
-  fit_file <- paste0(dat_dir, "draws_", sprintf("%03d", k), ".rds")
-  fit_dat <- readRDS(fit_file) %>%
-    rename(setNames(paste0("means[", seq_along(free_params), "]"),
-                    free_params))
-
-  for (p in free_params) {
-    sim_params[[p]][k] <- sim_dat[[p]]
-    fit_params[[p]][k] <- median(fit_dat[[p]])
-  }
+  plot$many_runs_param_fit(sim_params, fit_params, free_params)
 }
-
-plot$many_runs_param_fit(sim_params, fit_params, free_params)
